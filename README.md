38b6a8e615fe91de04ab4737d058de9036e9a8b5
doooghan <doooghan@foxmail.com>
2023-01-18 22:06:02 +0800

    app css

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ react + ts + vite + pnpm
 2. react 设计理念
    1. 单向数据流
    2. 虚拟 dom
+3. css 模组化（css module）
+   1. 使用 \*.d.ts 来定义
+   2. jss 模块化引入组件
+   3. 使用 typescript-plugin-css-modules 来做 css 模块化的 ts 提示
+4.
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 import robtos from "./mock/robots.json";
 import Robot from "./components/Robot";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <ul>
-      {robtos.map((r) => (
-        <Robot id={r.id} name={r.name} email={r.email} />
-      ))}
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
+        {robtos.map((r) => (
+          <Robot id={r.id} name={r.name} email={r.email} />
+        ))}
+      </div>
+    </div>
   );
 }
 
