172c720bc3ff9d0cd3fc87bfc71775f2509c780b
doooghan <doooghan@foxmail.com>
2023-02-12 12:21:54 +0800

    使用class

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import robtos from "./mock/robots.json";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={reactLogo} alt="logo" className={styles.appLogo} />
         <h1>吊炸天</h1>
       </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
         {robtos.map((r) => (
           <Robot id={r.id} name={r.name} email={r.email} />
