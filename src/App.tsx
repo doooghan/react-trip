5e4e741f4aa433d5778a3d905b7d71d7c1592ee4
doooghan <doooghan@foxmail.com>
2023-02-21 23:13:03 +0800

    首页

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
+import { Layout, Typography, Input } from "antd";
+import logo from "./assets/react.svg";
 import styles from "./App.module.css";
+
+const { Header } = Layout;
+const { Title } = Typography;
 
 interface Props {}
 interface State {}
 
 const App: React.FC = (props) => {
-  return <div className={styles.App}>test</div>;
+  return (
+    <div className={styles.App}>
+      <div>
+        <Header>
+          <img src={logo} alt="" />
+          <Title level={3}>欢迎来到 react trip</Title>
+          <Input placeholder={"请搜索"} />
+        </Header>
+      </div>
+    </div>
+  );
 };
 
 export default App;
