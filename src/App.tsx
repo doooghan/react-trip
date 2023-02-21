7be56c11afe043ef972aa52db7dee5198565c8f8
doooghan <doooghan@foxmail.com>
2023-02-21 23:02:25 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+import styles from "./App.module.css";
+
+interface Props {}
+interface State {}
+
+const App: React.FC = (props) => {
+  return <div className={styles.App}>test</div>;
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Shopping from "./Shopping/main";
+// import App from "./Shopping/main";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Shopping />
+    <App />
   </React.StrictMode>
 );
