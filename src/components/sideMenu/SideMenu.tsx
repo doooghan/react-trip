6469eb3bca9fca22d65d0e13a66c354fd98f2ff5
doooghan <doooghan@foxmail.com>
2023-03-11 17:23:37 +0800

    fix(sidemenu): 修复控制台报错

--- src/components/sideMenu/SideMenu.tsx
+++ src/components/sideMenu/SideMenu.tsx
@@ -1,46 +1,31 @@
 import React from "react";
 import styles from "./SideMenu.module.css";
-import { sideMenuList } from "../../../mock/mockup";
+import { sideMenuList } from "@/../mock/mockup";
 import { Menu } from "antd";
 import { GifOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+
+type MenuItem = Required<MenuProps>["items"][number];
 
 export const SideMenu: React.FC = () => {
-  return (
-    <Menu mode="vertical" className={styles["side-menu"]}>
-      {sideMenuList.map((m, index) => {
-        return (
-          <Menu.SubMenu
-            key={`side-menu-${index}`}
-            title={
-              <span>
-                <GifOutlined />
-                {m.title}
-              </span>
-            }
-          >
-            {m.subMenu.map((sm, smindex) => (
-              <Menu.SubMenu
-                key={`${index}-${smindex}`}
-                title={
-                  <span>
-                    <GifOutlined></GifOutlined>
-                    {sm.title}
-                  </span>
-                }
-              >
-                {sm.subMenu.map((sms, smsindex) => (
-                  <Menu.Item key={`${index}-${smindex}-${smsindex}`}>
-                    <span>
-                      <GifOutlined></GifOutlined>
-                      {sms}
-                    </span>
-                  </Menu.Item>
-                ))}
-              </Menu.SubMenu>
-            ))}
-          </Menu.SubMenu>
-        );
-      })}
-    </Menu>
-  );
+  const items: MenuItem[] = sideMenuList.map((m, index) => {
+    return {
+      label: m.title,
+      key: `side-menu-${index}`,
+      icon: <GifOutlined />,
+      children: m.subMenu.map((sm, smIndex) => {
+        return {
+          label: sm.title,
+          key: `${index}-${smIndex}`,
+          children: sm.subMenu.map((sms, smsIndex) => {
+            return {
+              label: sms,
+              key: `${index}-${smIndex}-${smsIndex}`,
+            };
+          }),
+        };
+      }),
+    };
+  });
+  return <Menu mode="vertical" className={styles["side-menu"]} items={items} />;
 };
